9408ee631caaad5d84fa399d3a43d98723612804
Senarathna.K.H <kaveeshaheshan590@gmail.com>
2025-01-28 22:08:38 +0530

    update

--- Frontend/Food-app/src/App.jsx
+++ Frontend/Food-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Cart from './pages/Cart/Cart'; // Ensure this import is correct
 import Footer from './components/Footer/Footer';
+
 
 const App = () => {
   return (
